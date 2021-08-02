df6ad385e1eb318378ab2d4699455e372ff30754
Ross Larson <zo0o0ot@gmail.com>
2021-08-02 05:43:04 +0000

    add draft data to startup.cs

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -10,6 +10,7 @@ namespace Api
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddSingleton<IProductData, ProductData>();
+            builder.Services.AddSingleton<IDraftData, DraftData>();
         }
     }
 }
